9c3448b829badeb1cc6c89636177d8950c225cfe
Vitya Chepovoi <48647850+vchepovoi@users.noreply.github.com>
2020-03-21 02:46:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var arr = str.split("");
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (arr[i] == bracketsConfig[j][0] && arr[i+1] == bracketsConfig[j][1]) {
+        arr.splice(i, 2);
+        i = 0;
+        j = 0;
+      }
+    }
+  }
+  if (arr.length == 0) {
+    return true;
+  }
+  return false;
 }
